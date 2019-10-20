05b333897e7f371f6d184495e30e6a329e12a8eb
Jas Spencer <jas.spencer414@gmail.com>
2019-10-20 23:44:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
+
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarf, index|
+    puts "${index + 1}. #{dwarf}"
+   end  # Your code here
 end
 
 def summon_captain_planet# code an argument here
