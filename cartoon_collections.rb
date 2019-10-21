35fe1965263b80e1429cf173d3649b9172cb6633
Jas Spencer <jas.spencer414@gmail.com>
2019-10-21 00:13:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ dwarves = %w[Doc Dopey Bashful Grumpy Sneezy Sleepy Happy]
 
 def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, index|
-    puts "${index + 1}. #{dwarf}"
+    puts "#{index + 1}. #{dwarf}"
    end  # Your code here
 end
 
