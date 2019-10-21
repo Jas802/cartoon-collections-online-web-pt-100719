7572b11174a2492bae15bc6fd4f52092ce85af18
Jas Spencer <jas.spencer414@gmail.com>
2019-10-21 00:06:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,7 +35,7 @@ def find_the_cheese(potential_cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   potential_cheese.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
+    cheese_types?(maybe_cheese)
   end
 end
 
